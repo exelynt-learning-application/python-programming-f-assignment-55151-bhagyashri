9dd69a03a16163be2f238792e6b4913d8ec45422
Bhagyashri-Dhavane <bhagyashri.dhavane@multigenesys.com>
2026-03-18 11:45:23 +0530

    for loop

---
+++  for_loop_numbers.py
@@ -0,0 +1,6 @@
+# This program prints numbers from 1 to 20 using a for loop
+
+# Using a for loop to iterate over a range from 1 to 20
+for number in range(1, 21):
+    # Printing each number on a separate line
+    print(number)

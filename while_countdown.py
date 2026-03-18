778de3f31c502dbf64d5c6f65379ab8d452551b2
Bhagyashri-Dhavane <bhagyashri.dhavane@multigenesys.com>
2026-03-18 11:48:46 +0530

    python while loop

---
+++ while_countdown.py
@@ -0,0 +1,12 @@
+# while_countdown.py
+
+# Initialize the counter variable to 10
+counter = 10
+
+# Using a while loop to decrement the counter
+while counter > 0:
+    print(counter)  # Printing each value in the countdown
+    counter -= 1  # Decrement the counter by 1
+
+# Final message after the countdown
+print("Blast off!")
